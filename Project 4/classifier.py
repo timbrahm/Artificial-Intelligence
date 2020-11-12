3f0a65e398107ce10125a02845f2c728fe9f757b
timbrahm <bratn-21@rhodes.edu>
2020-11-12 00:37:19 -0600

    project 4 updates and bug fixes

--- Project 4/classifier.py
+++ Project 4/classifier.py
@@ -1,47 +1,200 @@
-from itertools import islice
-
+import numpy as np
 
 def txtCheck(filename):
     if filename[-4:] != ".txt":
         filename += ".txt"
     return filename
 
-def parseEmail(word_list):
-    word_dict = {}
-    email_count = 0
-    start_indx = [i for i, v in enumerate(word_list) if v == "<SUBJECT>"]
-    end_indx = [i for i, v in enumerate(word_list) if v == "</BODY>"]
-    comb_indx = [(start_indx[i] + 1, end_indx[i]) for i in range(len(start_indx))]
-    email_list = [list(islice(word_list, a, b)) for a, b in comb_indx]
-    for i in range(len(email_list)):
-        email_list[i] = list(filter(lambda a: a != "</SUBJECT>" and a != "<BODY>", email_list[i]))
-    for item in email_list:
-        temp = set()
-        # print(item)
-        gen = (line.split() for line in item if len(line) > 0)
-        for line in gen:
-            print(line)
-            for word in line:
-                temp.add(word.lower())
-        print(list(temp))
-        print()
-
-
-    return word_dict, email_count
-
 class Classifier:
-    def __init__(self, file_list):
+    def __init__(self, file_list, DEBUG):
+        self.DEBUG = DEBUG
+
         self.spam_train_file, self.ham_train_file, self.spam_test_file, self.ham_test_file = file_list
 
-        self.train()
+        self.spam_dict, self.email_count_spam_train, self.ham_dict, self.email_count_ham_train = self.train()
+
+        self.Pspam = self.email_count_spam_train / (self.email_count_spam_train + self.email_count_ham_train)
+        self.Pham = self.email_count_ham_train / (self.email_count_spam_train + self.email_count_ham_train)
+
+        self.feat_given_spam, self.feat_given_ham = self.featureProbs()
 
     def train(self):
-        spam_list = open(txtCheck(self.spam_train_file), "r").read().splitlines()
-        spam_dict, email_count_spamt = parseEmail(spam_list)
-        # print(spam_dict)
-        # print(email_count_spamt)
+        spam_dict = {}
+        email_count_spam_train = 0
+        with open(txtCheck(self.spam_train_file), "r") as file:
+            email_set = set()
+            for line in file:
+                line = line.rstrip()
+                if line == "<SUBJECT>":
+                    email_count_spam_train += 1
+                    email_set = set()
+                elif line == "</BODY>":
+                    email_list = list(email_set)
+                    for word in email_list:
+                        word = word.lower()
+                        spam_dict[word] = spam_dict.get(word, 0) + 1
+                elif line != "</SUBJECT>" and line != "<BODY>" and line != "":
+                    line = line.split()
+                    for word in line:
+                        word = word.lower()
+                        email_set.add(word)
+            file.close()
 
-        # ham_list = open(txtCheck(self.ham_train_file), "r").read().splitlines()
-        # ham_dict, email_count_hamt = parseEmail(ham_list)
-        # print(ham_dict)
-        # print(email_count_hamt)
+        ham_dict = dict.fromkeys(spam_dict, 0)
+        email_count_ham_train = 0
+        with open(txtCheck(self.ham_train_file), "r") as file:
+            email_set = set()
+            for line in file:
+                line = line.rstrip()
+                if line == "<SUBJECT>":
+                    email_count_ham_train += 1
+                    email_set = set()
+                elif line == "</BODY>":
+                    email_list = list(email_set)
+                    for word in email_list:
+                        word = word.lower()
+                        try:
+                            ham_dict[word] += 1
+                        except KeyError:
+                            pass
+                elif line != "</SUBJECT>" and line != "<BODY>" and line != "":
+                    line = line.split()
+                    for word in line:
+                        word = word.lower()
+                        email_set.add(word)
+            file.close()
+        return spam_dict, email_count_spam_train, ham_dict, email_count_ham_train
+
+    def featureProbs(self):
+        feat_given_spam = {}
+        feat_given_ham = {}
+        for feature in self.spam_dict:
+            feat_given_spam[feature] = (self.spam_dict[feature] + 1) / (self.email_count_spam_train + 2)
+        for feature in self.ham_dict:
+            if (self.ham_dict[feature] + 1) / (self.email_count_ham_train + 2) >= 1:
+                print("ERROR")
+            feat_given_ham[feature] = (self.ham_dict[feature] + 1) / (self.email_count_ham_train + 2)
+        return feat_given_spam, feat_given_ham
+
+    def test(self, type_check):
+        if type_check == "SPAM":
+            email_count_spam_test = 0
+            correct_spam_count = 0
+            with open(txtCheck(self.spam_test_file), "r") as file:
+                feat_dict = {}
+                for line in file:
+                    line = line.rstrip()
+                    if line == "<SUBJECT>":
+                        email_count_spam_test += 1
+
+                        if self.DEBUG:
+                            print("Test email {}".format(email_count_spam_test))
+                            print("priors= {} {}".format(self.Pspam, self.Pham))
+
+                        feat_dict = dict.fromkeys(self.spam_dict, False)
+                    elif line == "</BODY>":
+                        ##For SPAM
+                        total_spam_prob = self.Pspam
+                        total_spam_prob_log = np.log(self.Pspam)
+                        for feat in feat_dict:
+                            feat_prob = self.feat_given_spam[feat]
+                            if not feat_dict[feat]:
+                                feat_prob = 1 - feat_prob
+                            total_spam_prob *= feat_prob
+                            total_spam_prob_log += np.log(feat_prob)
+                            if feat_prob <= 0:
+                                print("WARNING: {}".format(feat_prob))
+
+                        ##For HAM
+                        total_ham_prob = self.Pham
+                        total_ham_prob_log = np.log(self.Pham)
+                        for feat in feat_dict:
+                            feat_prob = self.feat_given_ham[feat]
+                            if not feat_dict[feat]:
+                                feat_prob = 1 - feat_prob
+                            if feat_prob <= 0:
+                                print("WARNING: {}".format(feat_prob))
+                                print("FEAT: {}".format(feat))
+                            total_ham_prob *= feat_prob
+                            total_ham_prob_log += np.log(feat_prob)
+
+
+                        if self.DEBUG:
+                            print("probs= {:.4f} {:.4f}".format(total_spam_prob, total_ham_prob))
+
+                        if total_spam_prob_log > total_ham_prob_log:
+                            spam_class = "SPAM"
+                            correct = "right"
+                            correct_spam_count += 1
+                        else:
+                            spam_class = "HAM"
+                            correct = "wrong"
+                        true_count = sum(feat_dict.values())
+                        print("TEST {} {}/{} features true {:.3f} {:.3f} {} {}".format(email_count_spam_test, true_count, len(feat_dict), total_spam_prob_log, total_ham_prob_log, spam_class.lower(), correct))
+
+                    elif line != "</SUBJECT>" and line != "<BODY>" and len(line) > 0:
+                        line = line.split()
+                        for word in line:
+                            if word in feat_dict:
+                                feat_dict[word] = True
+                file.close()
+            return correct_spam_count, email_count_spam_test
+        else:
+            email_count_ham_test = 0
+            correct_ham_count = 0
+            with open(txtCheck(self.ham_test_file), "r") as file:
+                feat_dict = {}
+                for line in file:
+                    line = line.rstrip()
+                    if line == "<SUBJECT>":
+                        email_count_ham_test += 1
+
+                        if self.DEBUG:
+                            print("Test email {}".format(email_count_ham_test))
+                            print("priors= {} {}".format(self.Pspam, self.Pham))
+
+                        feat_dict = dict.fromkeys(self.spam_dict, False)
+                    elif line == "</BODY>":
+                        ##For SPAM
+                        total_spam_prob = self.Pspam
+                        total_spam_prob_log = np.log(self.Pspam)
+                        for feat in feat_dict:
+                            feat_prob = self.feat_given_spam[feat]
+                            if not feat_dict[feat]:
+                                feat_prob = 1 - feat_prob
+                            total_spam_prob *= feat_prob
+                            total_spam_prob_log += np.log(feat_prob)
+
+                        ##For HAM
+                        total_ham_prob = self.Pham
+                        total_ham_prob_log = np.log(self.Pham)
+                        for feat in feat_dict:
+                            feat_prob = self.feat_given_ham[feat]
+                            if not feat_dict[feat]:
+                                feat_prob = 1 - feat_prob
+                            total_ham_prob *= feat_prob
+                            total_ham_prob_log += np.log(feat_prob)
+
+                        if self.DEBUG:
+                            print("probs= {:.4f} {:.4f}".format(total_spam_prob, total_ham_prob))
+
+                        if total_spam_prob_log > total_ham_prob_log:
+                            spam_class = "SPAM"
+                            correct = "wrong"
+                        else:
+                            spam_class = "HAM"
+                            correct = "right"
+                            correct_ham_count += 1
+                        true_count = sum(feat_dict.values())
+                        print("TEST {} {}/{} features true {:.3f} {:.3f} {} {}".format(email_count_ham_test, true_count,
+                                                                                       len(feat_dict), total_spam_prob_log,
+                                                                                       total_ham_prob_log,
+                                                                                       spam_class.lower(), correct))
+
+                    elif line != "</SUBJECT>" and line != "<BODY>" and len(line) > 0:
+                        line = line.split()
+                        for word in line:
+                            if word in feat_dict:
+                                feat_dict[word] = True
+                file.close()
+            return correct_ham_count, email_count_ham_test
--- Project 4/main.py
+++ Project 4/main.py
@@ -1,18 +1,74 @@
 from classifier import Classifier
 
 
+DEBUG = False
+
 def main():
     # spam_train_file = input("Please enter a training file for spam: ")
     # ham_train_file = input("Please enter a training file for ham: ")
     # spam_test_file = input("Please enter a testing file for spam: ")
     # ham_test_file = input("Please enter a testing file for ham: ")
-    spam_train_file = "train-spam-small.txt"
-    ham_train_file = "train-ham-small.txt"
-    spam_test_file = "test-spam-small.txt"
-    ham_test_file = "test-ham-small.txt"
+
+    # spam_train_file = "train-spam-small.txt"
+    # ham_train_file = "train-ham-small.txt"
+    # spam_test_file = "test-spam-small.txt"
+    # ham_test_file = "test-ham-small.txt"
+
+    spam_train_file = "train-spam.txt"
+    ham_train_file = "train-ham.txt"
+    spam_test_file = "test-spam.txt"
+    ham_test_file = "test-ham.txt"
+
+
     file_list = [spam_train_file, ham_train_file, spam_test_file, ham_test_file]
 
-    spam_classifier = Classifier(file_list)
+    spam_classifier = Classifier(file_list, DEBUG)
+
+    with open(ham_train_file, "r") as file:
+        for line in file:
+            line = line.rstrip()
+            line = line.split()
+            for word in line:
+                if word != "<BODY>" and word != "</SUBJECT>" and word != "</BODY>" and word != "<SUBJECT>":
+                    word = word.lower()
+                    if word not in spam_classifier.spam_dict:
+                            print(word)
+        file.close()
+
+
+
+
+    print(len(spam_classifier.feat_given_spam))
+    print(len(spam_classifier.feat_given_ham))
+    print(len(spam_classifier.spam_dict))
+    print(len(spam_classifier.ham_dict))
+
+    if DEBUG:
+        print("Training from {} and {}".format(spam_train_file, ham_train_file))
+        print("Testing from {} and {}".format(spam_test_file, ham_test_file))
+        print("number of emails: {} vs {}".format(spam_classifier.email_count_spam_train, spam_classifier.email_count_ham_train))
+        print("entire vocab: {}".format(list(spam_classifier.spam_dict.keys())))
+        print("entire vocab size is: {}".format(len(spam_classifier.spam_dict.keys())))
+        print("spam words: {}".format(spam_classifier.spam_dict))
+        print("ham words: {}".format(spam_classifier.ham_dict))
+        print("Beginning tests.")
+        print("Testing spam emails.")
+
+    correct_spam, total_spam = spam_classifier.test("SPAM")
+
+    if DEBUG:
+        print("{} out of {} classified correctly.".format(correct_spam, total_spam))
+
+        print("Testing ham emails.")
+
+    correct_ham, total_ham = spam_classifier.test("HAM")
+
+    if DEBUG:
+        print("{} out of {} classified correctly.".format(correct_ham, total_ham))
+
+    correct = correct_spam + correct_ham
+    total = total_spam + total_ham
+    print("Total: {}/{} emails classified correctly.".format(correct, total))
 
     return 0
 
